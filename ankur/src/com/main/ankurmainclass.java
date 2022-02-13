b9ea11b2eb669ec1a322bec8988167a4620e26f4
PRUDHVI80 <prudhvireddy.kurre@gmail.com>
2022-02-13 17:32:30 +0530

    second commite

---
+++ ankur/src/com/main/ankurmainclass.java
@@ -0,0 +1,16 @@
+package com.main;
+
+public class ankurmainclass {
+
+	public static void main(String[] args)
+	{
+		int h=2;
+		for(int i=1;i<=10;i++)
+		{
+		System.out.println(i+h);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
