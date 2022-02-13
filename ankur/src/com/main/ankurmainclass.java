1ee69e1f61b99f9d15ee1f334a045859d556f72a
PRUDHVI80 <prudhvireddy.kurre@gmail.com>
2022-02-13 17:32:30 +0530

    All squash Update ankurmainclass.java
    UPDATING H VALUE
    first commite
    Update ankurmainclass.java
    
    FIRST CHANGES IN ANKURBRANCH
    secon comm....ite

---
+++ ankur/src/com/main/ankurmainclass.java
@@ -0,0 +1,28 @@
+package com.main;
+
+public class ankurmainclass {
+
+	public static void main(String[] args)
+	{
+		int h=119;
+		for(int i=1;i<=10;i++)
+		{
+		System.out.println(i+h);
+		}
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+		{
+			int k=2;
+			for(int j=1;j<5;j++)
+			{
+				
+				System.out.print(j);
+				
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
